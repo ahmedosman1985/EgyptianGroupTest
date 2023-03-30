1a6f1b159146e1c83564d5c0781f1f551b1ffc0f
ahmedosman1985 <ahmed.osman.va@gmail.com>
2023-03-30 18:33:17 -0400

    osman copy

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("my name is Ahmed Osman");
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+        }
     }
 }
